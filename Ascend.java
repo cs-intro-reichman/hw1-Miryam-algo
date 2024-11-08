e15531440b6dd2a74af55b03fee24d118dbad0bc
miryam100 <miryam.marcus@post.runi.ac.il>
2024-11-08 10:43:37 +0200

    second commit - solution

--- Ascend.java
+++ Ascend.java
@@ -3,9 +3,9 @@
 public class Ascend {
 	public static void main(String[] args) {
 		int lim = Integer.parseInt(args[0]);
-		int a = (int)(Math.random()*lim);
-		int b = (int)(Math.random()*lim);
-		int c = (int)(Math.random()*lim);
+		int a = (int)(Math.random()*(lim+1));
+		int b = (int)(Math.random()*(lim+1));
+		int c = (int)(Math.random()*(lim+1));
 		System.out.println (a +" "+ b+" "+c);
 		int mini = Math.min(a,b);
 		mini = Math.min(mini,c);
